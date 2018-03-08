b9e90737519d29867fdb2af9dad5ab47b65648cd
saloppe73 <iizegrim@protonmail.com>
2018-03-08 22:00:34 +0100

    initial upload

---
+++ src/P2p/P2pNetworks.h
@@ -0,0 +1,25 @@
+// Copyright (c) 2012-2017, The CryptoNote developers, The Bytecoin developers
+//
+// This file is part of Bytecoin.
+//
+// Bytecoin is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Bytecoin is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with Bytecoin.  If not, see <http://www.gnu.org/licenses/>.
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid BYTECOIN_NETWORK = { {  0x40, 0xa7, 0xe5, 0x86, 0x38, 0x0c, 0x99, 0x84, 0x87, 0xc4, 0x31, 0xca, 0x27, 0xb2, 0xc2, 0xe0  } };
+
+}
+
